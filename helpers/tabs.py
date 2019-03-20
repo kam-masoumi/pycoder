356dd9e74c10bf11b39edd523cdb5ac82c5c3db0
kamran masoumi <kam.masoumi@gmail.com>
2019-03-21 01:14:01 +0330

    Enhancement create tabs and highlighter.

--- helpers/tabs.py
+++ helpers/tabs.py
@@ -1,7 +1,7 @@
 from linecache import cache
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QTabWidget, QGridLayout
+from PyQt5.QtWidgets import QTabWidget, QGridLayout, QTextEdit
 
 from helpers.highlighter import Highlighter
 from helpers.text_editor import TextEditor
@@ -19,7 +19,6 @@ class Tabs:
     def createTab(self, data, file_name, directory):
         oldFileDirectory = cache.get(file_name)
         allFile = cache.keys()
-
         if file_name in allFile and oldFileDirectory == directory:
             return False
         elif file_name in allFile and oldFileDirectory != directory:
@@ -28,24 +27,31 @@ class Tabs:
             pass
 
         tabLayout = QGridLayout()
-        newTab = TextEditor.createTextEditor()
-        self.highlighter = Highlighter(newTab.document())
+        newTab = TextEditor().createTextEditor()
         newTab.setText(data)
 
         tabLayout.addWidget(newTab)
-
         self.tabs.addTab(newTab, f'{file_name}')
         tabIndex = self.tabs.indexOf(newTab)
         self.tabs.setTabIcon(tabIndex, QIcon('python1.png'))
         self.tabs.tabBarClicked.connect(self.changeStatusBar)
         cache.setdefault(file_name, directory)
 
+        self.tabs.setCurrentWidget(newTab)
+        self.changeStatusBar(-1)
         self.setCentralWidget(self.tabs)
+        self.statusBar().showMessage(directory)
 
     def closeTab(self, index):
         currentTabName = self.tabs.tabText(index)
-        cache.pop(currentTabName)
+        try:
+            cache.pop(currentTabName)
+        except KeyError:
+            pass
         self.tabs.removeTab(index)
+        newCurrentTabName = self.tabs.tabText(0)
+        currentDirectory = cache.get(newCurrentTabName)
+        self.statusBar().showMessage(currentDirectory)
         return index
 
     def changeStatusBar(self, index):
