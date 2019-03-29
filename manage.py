dcf88bfd6c82cb08f38029354acc3c3d32a50f67
kamran masoumi <kam.masoumi@gmail.com>
2019-03-29 22:10:22 +0430

    Create login

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
 import inspect
 import sys
 
-from PyQt5.QtWidgets import QApplication
+import time
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen, QProgressBar
 
 import models
 from main import MainWindow
@@ -19,6 +22,36 @@ def create_all_database():
 if __name__ == '__main__':
     create_all_database()
     app = QApplication(sys.argv)
+
+    splash_pix = QPixmap('images/loading.jpg')
+
+    splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+    splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splash.setEnabled(False)
+
+    progressBar = QProgressBar(splash)
+    progressBar.setStyleSheet("""
+    QProgressBar {
+    text-align: center;
+    }
+    QProgressBar::chunk {
+    background-color: rgb(49,49,49);
+    width: 10px;
+    }
+    """)
+    progressBar.setMaximum(10)
+    progressBar.setGeometry(0, splash_pix.height() - 17, splash_pix.width(), 20)
+
+    splash.show()
+    splash.showMessage("<h1><font color='white'>Enjoy from this editor</font></h1>", Qt.AlignBottom | Qt.AlignCenter)
+
+    for i in range(1, 10):
+        progressBar.setValue(i)
+        t = time.time()
+        while time.time() < t + 0.1:
+            app.processEvents()
+
     gui = MainWindow()
     gui.show()
+    splash.finish(gui)
     sys.exit(app.exec_())
